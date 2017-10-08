4c6f3e36c7f733e479af20a2c03cf62fed9f5fd2
matheusPeresDeAraujo <matheus.peres.araujo@gmail.com>
2017-10-08 19:43:01 -0300

    add-sort

---
+++ src/model/ResolveVetorCollisionReductionSort.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class ResolveVetorCollisionReductionSort implements ResolveVetor{
+
+	public static int instancia = 0;
+	
+	@Override
+	public Vetor soluciona(Vetor vetor) {
+
+		if(vetor.getColisoes() < 1 || instancia > 3000)
+			return vetor;
+		
+		List<Vetor> vetores = new ArrayList<>();
+		vetores.add(vetor);
+		
+		vetor = new Vetor(this.solucionar(vetores).get(0).getVetor());
+		
+		return vetor;
+	}
+	
+	private List<Vetor> solucionar(List<Vetor> vetores){
+		
+		//Gero filhos para o primeiro index.
+		AlteraPosicaoVetorFilhos filhos = new AlteraPosicaoVetorFilhos1Posicao();
+		Random k = new Random();
+		int p = k.nextInt(vetores.size())/3;
+		List<Vetor> filhosLista = filhos.altera(vetores.get(p));
+		vetores.remove(p);
+		
+		if(filhosLista.get(0).getColisoes() < 1 || instancia > 3000) {
+			return filhosLista;
+		}
+		
+		// Junto as duas lista para omitir as repetições
+		Map<int[], Vetor> merge = new HashMap<>();
+		for(Vetor vetor : vetores) {
+			merge.put(vetor.getVetor(), vetor);
+		}
+		for(Vetor filho : filhosLista) {
+			merge.put(filho.getVetor(), filho);
+		}
+		
+		filhosLista.clear();
+		
+		filhosLista.addAll(merge.values());
+		
+		
+		instancia++;
+		System.out.println(instancia);
+		
+		return solucionar(filhosLista);
+		
+	}
+	
+}
